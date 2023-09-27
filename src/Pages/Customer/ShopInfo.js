a2d231daa8c09dde79e1425fc667788f81e063f0
krystalvv <p.krystalvv@gmail.com>
2023-09-28 05:21:07 +0900

    temp

--- src/Pages/Customer/ShopInfo.js
+++ src/Pages/Customer/ShopInfo.js
@@ -1,4 +1,4 @@
-import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Alert, AlertIcon, Badge, Box, Button, Card, CardHeader, Center, Checkbox, CheckboxGroup, Circle, Flex, HStack, Heading, Image, Input, InputGroup, InputRightAddon, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Select, Stack, Tag, TagCloseButton, TagRightIcon, Text, Textarea, VStack, Wrap, WrapItem, useDisclosure } from "@chakra-ui/react";
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Alert, AlertIcon, Badge, Box, Button, Card, CardHeader, Center, Checkbox, CheckboxGroup, Circle, Container, Flex, HStack, Heading, Image, Input, InputGroup, InputRightAddon, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Select, Stack, Tag, TagCloseButton, TagRightIcon, Text, Textarea, VStack, Wrap, WrapItem, useDisclosure } from "@chakra-ui/react";
 import React, { useCallback, useEffect, useState } from "react";
 import { Body_lg, Body_sm, Title_2xl, Title_lg, Title_sm } from "../../Style/Typograhy";
 import { addDocument, formattedAmount, getDocument, getShop, updateData } from "../../DB/function";
@@ -52,53 +52,57 @@ const ShopInfo = () => {
     }, []);
 
     return (
-        <>
-            <Flex w='100%' left={0} position="fixed" zIndex={999} borderBottom={'1px solid #d9d9d9'}>
+        <Box w='100%'>
+            <Flex w='100%' position={'fixed'} zIndex={999} borderBottom={'1px solid #d9d9d9'}>
                 <MobileStatus title={"상점정보"} />
             </Flex>
-            <Flex mt={'50px'} display={'block'} bgColor={'white'} position={'fixed'}>
-                {/* <Text w="100%" textAlign={'center'} p={2} color={'gray.500'}>미리보기</Text> */}
-                {account.profileImage ? <Image src={account.profileImage} bgColor={'gray.300'} w='400px' h='400px' /> : <Box w={'400px'} h={'400px'} bgColor={'gray.300'} />}
-                <Center mt={"-150px"}>
-                    <Circle w={'300px'} h="300px" bgColor={'white'}>
-                        <VStack spacing={0} mt={'-100px'}>
-                            <HStack alignItems={'center'}>
-                                <Badge mr={2} variant={'outline'}>{account.grade}</Badge>
-                                <Text {...Title_lg} mb={0}>{shopInfo.shopname}</Text>
-                            </HStack>
-                            <Text {...Body_lg}>{shopInfo.nickname}</Text>
+            <Flex maxW={'container.sm'} w='100%' justifyContent={'center'}>
 
-                            <HStack alignItems={'center'}>
-                                <Text {...Body_lg}>팔로워 {shopInfo.follower ? shopInfo.follower.length : 0} | 리뷰 {shopInfo.review ? shopInfo.review.length : 0}</Text>
-                            </HStack>
-                        </VStack>
-                    </Circle>
-                </Center>
-                <Box bgColor={'white'} mt={'-150px'} p={4}>
-                    <Label title={'카테고리'} description={shopInfo.category} />
-                    <Label title={'배송'} description={shopInfo.delivery} />
-                    {shopInfo.use_bundle_delivery && <Label title={'묶음배송'} text={formattedAmount(shopInfo.bundle_delivery) + "원 까지 묶음배송"} />}
-                    <Label title={'위치'} text={account.address + " " + account.detail_address} />
-                    <Label title={'운영시간'} text={(!onlyweekend ? "평일 " + shopInfo.operate_time.week_open + "~" + shopInfo.operate_time.week_close : "") + (!onlyweek && !onlyweekend ? " | " : "") + (!onlyweek ? ("주말 " + shopInfo.operate_time.weekend_open + "~" + shopInfo.operate_time.weekend_close) : "")} />
-                    <Label title={'예약시간'} text={(!onlyweekend ? "평일 " + shopInfo.reserve_time.week_open + "~" + shopInfo.reserve_time.week_close : "") + (!onlyweek && !onlyweekend ? " | " : "") + (!onlyweek ? ("주말 " + shopInfo.reserve_time.weekend_open + "~" + shopInfo.reserve_time.weekend_close) : "")} />
-                    <Label title={'브레이크타임'} text={(!onlyweekend ? "평일 " + shopInfo.break_time.week_open + "~" + shopInfo.break_time.week_close : "") + (!onlyweek && !onlyweekend ? " | " : "") + (!onlyweek ? ("주말 " + shopInfo.break_time.weekend_open + "~" + shopInfo.break_time.weekend_close) : "")} />
-                    <Label title={'휴무일'} description={shopInfo.holiday} />
-                    <Label title={'SNS'} children=
-                        {
-                            <Stack direction={'column'} spacing={0}>
+                <Flex mt={'50px'} display={'block'} bgColor={'white'}>
+                    {/* <Text w="100%" textAlign={'center'} p={2} color={'gray.500'}>미리보기</Text> */}
+                    {account.profileImage ? <Image src={account.profileImage} bgColor={'gray.300'} w='400px' h='400px' /> : <Box w={'400px'} h={'400px'} bgColor={'gray.300'} />}
+                    <Center mt={"-150px"}>
+                        <Circle w={'300px'} h="300px" bgColor={'white'}>
+                            <VStack spacing={0} mt={'-100px'}>
+                                <HStack alignItems={'center'}>
+                                    <Badge mr={2} variant={'outline'}>{account.grade}</Badge>
+                                    <Text {...Title_lg} mb={0}>{shopInfo.shopname}</Text>
+                                </HStack>
+                                <Text {...Body_lg}>{shopInfo.nickname}</Text>
 
-                                {shopInfo.sns.map((value, index) => (
-                                    <Text {...Body_sm} mx={-1}>{value.site} {" | "} {value.account}</Text>
-                                ))}
-                            </Stack>
-                        }
-                    />
-                    <Label title={'Email'} text={shopInfo.email} />
-                    <Label title={'소개'} text={shopInfo.comment} />
-                </Box>
+                                <HStack alignItems={'center'}>
+                                    <Text {...Body_lg}>팔로워 {shopInfo.follower ? shopInfo.follower.length : 0} | 리뷰 {shopInfo.review ? shopInfo.review.length : 0}</Text>
+                                </HStack>
+                            </VStack>
+                        </Circle>
+                    </Center>
+                    <Box bgColor={'white'} mt={'-150px'} p={4}>
+                        <Label title={'카테고리'} description={shopInfo.category} />
+                        <Label title={'배송'} description={shopInfo.delivery} />
+                        {shopInfo.use_bundle_delivery && <Label title={'묶음배송'} text={formattedAmount(shopInfo.bundle_delivery) + "원 까지 묶음배송"} />}
+                        <Label title={'위치'} text={account.address + " " + account.detail_address} />
+                        <Label title={'운영시간'} text={(!onlyweekend ? "평일 " + shopInfo.operate_time.week_open + "~" + shopInfo.operate_time.week_close : "") + (!onlyweek && !onlyweekend ? " | " : "") + (!onlyweek ? ("주말 " + shopInfo.operate_time.weekend_open + "~" + shopInfo.operate_time.weekend_close) : "")} />
+                        <Label title={'예약시간'} text={(!onlyweekend ? "평일 " + shopInfo.reserve_time.week_open + "~" + shopInfo.reserve_time.week_close : "") + (!onlyweek && !onlyweekend ? " | " : "") + (!onlyweek ? ("주말 " + shopInfo.reserve_time.weekend_open + "~" + shopInfo.reserve_time.weekend_close) : "")} />
+                        <Label title={'브레이크타임'} text={(!onlyweekend ? "평일 " + shopInfo.break_time.week_open + "~" + shopInfo.break_time.week_close : "") + (!onlyweek && !onlyweekend ? " | " : "") + (!onlyweek ? ("주말 " + shopInfo.break_time.weekend_open + "~" + shopInfo.break_time.weekend_close) : "")} />
+                        <Label title={'휴무일'} description={shopInfo.holiday} />
+                        <Label title={'SNS'} children=
+                            {
+                                <Stack direction={'column'} spacing={0}>
+
+                                    {shopInfo.sns.map((value, index) => (
+                                        <Text {...Body_sm} mx={-1}>{value.site} {" | "} {value.account}</Text>
+                                    ))}
+                                </Stack>
+                            }
+                        />
+                        <Label title={'Email'} text={shopInfo.email} />
+                        <Label title={'소개'} text={shopInfo.comment} />
+                    </Box>
+
+                </Flex>
 
             </Flex>
-        </>
+        </Box>
     )
 }
 export default ShopInfo;
