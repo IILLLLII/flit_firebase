ee6ce7cc95587678f6851583f894a010b372c239
krystalvv <p.krystalvv@gmail.com>
2023-09-27 11:34:58 +0900

    imageslider

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export const isCustomer = window.location.pathname.includes('customer');
 function App() {
 
   return (
-    <Container maxW={isCustomer ? "100vw" : '100%'} p={isCustomer ?  0 : 4}>
+    <Container maxW={isCustomer ? "container.sm" : '100%'} p={isCustomer ?  0 : 4}>
       {isAdmin ? <SidebarWithHeader/> : isCustomer ? <CustomerApp/> : <SideBar/>}
       {/* <SideBar/> */}
     </Container>
